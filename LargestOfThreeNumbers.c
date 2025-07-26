a69b25499c8864221062ba4a09bcfdafab8c2977
JeodaPathan22 <jeodapathanp@gmail.com>
2025-07-26 17:07:49 +0600

    Add files via upload

---
+++ LargestOfThreeNumbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter three numbers = ");
+    scanf("%d %d %d",&x,&y,&z);
+    if(x>=y&&y>=z)
+    {
+        printf("%d is the largest number.",x);
+    }
+    else if(y>=x&&y>=z)
+    {
+        printf("%d is the largest number.",y);
+    }
+    else
+    {
+        printf("%d is the largest number.",z);
+    }
+    return 0;
+}
