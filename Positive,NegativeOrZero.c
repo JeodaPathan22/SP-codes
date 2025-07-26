0509b118dd95fd57eb1dbe27d397043081ccc714
JeodaPathan22 <jeodapathanp@gmail.com>
2025-07-26 20:39:00 +0600

    Add files via upload

---
+++ Positive,NegativeOrZero.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("%d is a Positive Number\n",a);
+    }
+    else if(a<0)
+    {
+        printf("%d is a Negative Number\n",a);
+    }
+    else
+    {
+        printf("this Number is-6 Zero\n",a);
+    }
+     return 0;
+}
